54271cb760a6538d99ec273a2e88e0220cf0776d
Rasulov RasulBek <rasulovrp@yandex.ru>
2025-06-09 15:45:11 +0300

    bug fix

--- src/managers/TaskManager.java
+++ src/managers/TaskManager.java
@@ -7,19 +7,29 @@ import java.util.List;
 
 public interface TaskManager {
     Task createTask(Task task);
+
     List<Task> getAllTasks();
+
     void deleteTask(int id);
+
     Task getTask(int id);
+
     void updateTask(Task task); // Добавлен новый метод
 
     Epic createEpic(Epic epic);
+
     List<Epic> getAllEpics();
+
     void deleteEpic(int id);
+
     Epic getEpic(int id);
 
     Subtask createSubtask(Subtask subtask);
+
     void updateSubtask(Subtask subtask);
+
     List<Subtask> getSubtasksByEpic(int epicId);
+
     Subtask getSubtask(int id);
 
     List<Task> getHistory();
