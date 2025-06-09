e1e453fcd1ff7049a456c2580443f9bce5d0132b
Rasulov RasulBek <rasulovrp@yandex.ru>
2025-06-09 15:52:59 +0300

    испраление ошибки: CheckStyle

--- src/model/Task.java
+++ src/model/Task.java
@@ -24,12 +24,19 @@ public class Task {
 
     // Геттеры и сеттеры
     public int getId() { return id; }
+
     public void setId(int id) { this.id = id; }
+
     public String getTitle() { return title; }
+
     public void setTitle(String title) { this.title = title; }
+
     public String getDescription() { return description; }
+
     public void setDescription(String description) { this.description = description; }
+
     public TaskStatus getStatus() { return taskStatus; }
+
     public void setStatus(TaskStatus taskStatus) { this.taskStatus = taskStatus; }
 
     @Override
