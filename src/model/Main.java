92f99a51318de1e62015735bb5f63f798dbb2b34
Rasulov RasulBek <rasulovrp@yandex.ru>
2025-06-10 13:51:11 +0300

    переместил класс маин в пакет модел

--- src/model/Main.java
+++ src/model/Main.java
@@ -1,9 +1,7 @@
+package model;
+
 import managers.Managers;
 import managers.TaskManager;
-import model.Epic;
-import model.Subtask;
-import model.Task;
-import model.TaskStatus;
 
 public class Main {
     public static void main(String[] args) {
